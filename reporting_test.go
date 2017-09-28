478f6e17e550136fe69012a48506b5dc492a9fb5
Ionut Milica <ionut.milica@gmail.com>
2017-09-28 12:01:41 +0300

    Added tests for custom period reports

--- reporting_test.go
+++ reporting_test.go
@@ -67,6 +67,50 @@ func TestReportProductDimension(t *testing.T) {
 
 }
 
+func TestReportProductDimensionWithCustomPeriod(t *testing.T) {
+	now := time.Now()
+	lastMonth := now.AddDate(0, -1, 0)
+	svc := reportingService()
+	accountId, _ := strconv.ParseInt(svc.Session.AccountId, 10, 64)
+	rr := &ProductDimensionPerformanceReportRequest{
+		Scope: ReportScope{
+			AccountIds: Longs{accountId},
+		},
+		Aggregation: "Daily",
+		Columns: []string{
+			"TimePeriod",
+			"AccountName",
+			"AccountNumber",
+			"AdGroupId",
+			"AdGroupName",
+			"CampaignName",
+			"DeviceType",
+			"Language",
+			"MerchantProductId",
+			"Title",
+			"Condition",
+			"Brand",
+			"Impressions",
+			"Clicks",
+			"Ctr",
+			"AverageCpc",
+			"Spend",
+			"Conversions",
+			"Revenue",
+		},
+		Time: ReportTime{
+			CustomDateRangeEnd:   Date{Year: now.Year(), Month: int(now.Month()), Day: now.Day()},
+			CustomDateRangeStart: Date{Year: lastMonth.Year(), Month: int(lastMonth.Month()), Day: lastMonth.Day()},
+		},
+	}
+	id, err := svc.SubmitReportRequest(rr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pollReport(t, svc, id)
+}
+
 func TestReportAdGroup(t *testing.T) {
 	svc := reportingService()
 	accountId, _ := strconv.ParseInt(svc.Session.AccountId, 10, 64)
@@ -94,6 +138,46 @@ func TestReportAdGroup(t *testing.T) {
 			"Revenue",
 		},
 		Time: ReportTime{PredefinedTime: "LastMonth"},
+	}
+	id, err := svc.SubmitReportRequest(rr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	svc = reportingService()
+	pollReport(t, svc, id)
+}
+
+func TestReportAdGroupWithCustomPeriod(t *testing.T) {
+	now := time.Now()
+	lastMonth := now.AddDate(0, -1, 0)
+	svc := reportingService()
+	accountId, _ := strconv.ParseInt(svc.Session.AccountId, 10, 64)
+	rr := &AdGroupPerformanceReportRequest{
+		Scope: ReportScope{
+			AccountIds: Longs{accountId},
+		},
+		Aggregation: "Daily",
+		Columns: []string{
+			"TimePeriod",
+			"AccountId",
+			"AccountName",
+			"AccountNumber",
+			"AdGroupId",
+			"AdGroupName",
+			"DeviceType",
+			"Impressions",
+			"Clicks",
+			"Ctr",
+			"AverageCpc",
+			"Spend",
+			"Conversions",
+			"Revenue",
+		},
+		Time: ReportTime{
+			CustomDateRangeEnd:   Date{Year: now.Year(), Month: int(now.Month()), Day: now.Day()},
+			CustomDateRangeStart: Date{Year: lastMonth.Year(), Month: int(lastMonth.Month()), Day: lastMonth.Day()},
+		},
 	}
 	id, err := svc.SubmitReportRequest(rr)
 	if err != nil {
